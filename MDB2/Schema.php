4a357e40d8446cb7ce2033ecea2a25ffd4cd8780
Lukas Smith <lsmith@php.net>
2005-08-14 11:04:12 +0000

    smith@backendmedia.com => smith@pooteeweet.org
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2_Schema/trunk@193399 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB2/Schema.php
+++ MDB2/Schema.php
@@ -39,7 +39,7 @@
 // | WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE          |
 // | POSSIBILITY OF SUCH DAMAGE.                                          |
 // +----------------------------------------------------------------------+
-// | Author: Lukas Smith <smith@backendmedia.com>                         |
+// | Author: Lukas Smith <smith@pooteeweet.org>                           |
 // +----------------------------------------------------------------------+
 //
 // $Id$
@@ -73,7 +73,7 @@ define('MDB2_SCHEMA_ERROR_NODBSELECTED', -6);
  *
  * @package MDB2_Schema
  * @category Database
- * @author  Lukas Smith <smith@backendmedia.com>
+ * @author  Lukas Smith <smith@pooteeweet.org>
  */
 class MDB2_Schema extends PEAR
 {
--- MDB2/Schema/Writer.php
+++ MDB2/Schema/Writer.php
@@ -39,7 +39,7 @@
 // | WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE          |
 // | POSSIBILITY OF SUCH DAMAGE.                                          |
 // +----------------------------------------------------------------------+
-// | Author: Lukas Smith <smith@backendmedia.com>                         |
+// | Author: Lukas Smith <smith@pooteeweet.org>                           |
 // +----------------------------------------------------------------------+
 //
 // $Id$
@@ -51,7 +51,7 @@
  * @package MDB2_Schema
  * @category Database
  * @access protected
- * @author  Lukas Smith <smith@backendmedia.com>
+ * @author  Lukas Smith <smith@pooteeweet.org>
  */
 class MDB2_Schema_Writer
 {
