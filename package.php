48595393a05722c09fc55f69a12994cbed05471f
Lukas Smith <lsmith@php.net>
2005-12-11 18:23:12 +0000

    - updated MDB2 dep
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2_Schema/trunk@202663 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -58,9 +58,9 @@ if (PEAR::isError($result)) {
 
 $package->addMaintainer('lsmith', 'lead', 'Lukas Kahwe Smith', 'smith@pooteeweet.org');
 
-$package->addDependency('php',       '4.2.0', 'ge',  'php', false);
-$package->addDependency('PEAR',      '1.0b1', 'ge',  'pkg', false);
-$package->addDependency('MDB2', '2.0.0beta7', 'ge',  'pkg', false);
+$package->addDependency('php',     '4.2.0', 'ge',  'php', false);
+$package->addDependency('PEAR',    '1.0b1', 'ge',  'pkg', false);
+$package->addDependency('MDB2', '2.0.0RC1', 'ge',  'pkg', false);
 $package->addDependency('XML_Parser',   true, 'has', 'pkg', false);
 $package->addDependency('XML_DTD',      true, 'has', 'pkg', true);
 
