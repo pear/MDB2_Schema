b3974b066e7878c6b5319e43a418ea8cec5e0cb8
Igor Feghali <ifeghali@php.net>
2006-08-22 00:53:20 +0000

    - removed detailed validate error codes - added error code MDB2_SCHEMA_ERROR_VALIDATE
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2_Schema/trunk@218614 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB2/Schema.php
+++ MDB2/Schema.php
@@ -62,50 +62,7 @@ define('MDB2_SCHEMA_ERROR_NOT_CAPABLE',                             -3);
 define('MDB2_SCHEMA_ERROR_UNSUPPORTED',                             -4);    // Driver does not support this function
 define('MDB2_SCHEMA_ERROR_INVALID',                                 -5);    // Invalid attribute value
 define('MDB2_SCHEMA_ERROR_NODBSELECTED',                            -6);
-define('MDB2_SCHEMA_ERROR_VALIDATE_AUTOINC_INVALID_DEFAULT',              -7);
-define('MDB2_SCHEMA_ERROR_VALIDATE_AUTOINC_NULL',                         -8);
-define('MDB2_SCHEMA_ERROR_VALIDATE_BROKEN_INDEX',                         -9);
-define('MDB2_SCHEMA_ERROR_VALIDATE_BROKEN_SEQUENCE',                      -10);
-define('MDB2_SCHEMA_ERROR_VALIDATE_DEFAULT_NOT_ALLOWED',                  -11);
-define('MDB2_SCHEMA_ERROR_VALIDATE_DML_FIELD_EXISTS',                     -12);
-define('MDB2_SCHEMA_ERROR_VALIDATE_DML_FIELD_NOT_FOUND',                  -13);
-define('MDB2_SCHEMA_ERROR_VALIDATE_DML_INVALID_FIELD_VALUE',              -14);
-define('MDB2_SCHEMA_ERROR_VALIDATE_DML_INVALID_VALUE_TYPE',               -15);
-define('MDB2_SCHEMA_ERROR_VALIDATE_DML_NEGATIVE_VALUE_FOR_SIGNED_FIELD',  -16);
-define('MDB2_SCHEMA_ERROR_VALIDATE_DML_NO_FIELD_NAME',                    -17);
-define('MDB2_SCHEMA_ERROR_VALIDATE_DML_VALUE_TOO_BIG',                    -18);
-define('MDB2_SCHEMA_ERROR_VALIDATE_DUP_AUTOINC',                          -19);
-define('MDB2_SCHEMA_ERROR_VALIDATE_FIELD_EXISTS',                         -20);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INDEX_EXISTS',                         -21);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_CREATE',                       -22);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_DATABASE_NAME',                -23);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_DEFAULT',                      -24);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_FIELD_NAME',                   -25);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_FIELD_TYPE',                   -26);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_FIXED',                        -27);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_LENGTH',                       -28);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_NOTNULL',                      -29);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_OVERWRITE',                    -30);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_PRIMARY',                      -31);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_SEQUENCE',                     -32);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_SEQUENCE_NAME',                -33);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_SORT',                         -34);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_TABLE_NAME',                   -35);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_UNIQUE',                       -36);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INVALID_UNSIGNED',                     -37);
-define('MDB2_SCHEMA_ERROR_VALIDATE_NO_DATABASE_NAME',                     -38);
-define('MDB2_SCHEMA_ERROR_VALIDATE_NO_FIELD_NAME',                        -39);
-define('MDB2_SCHEMA_ERROR_VALIDATE_NO_FIELD_TYPE',                        -40);
-define('MDB2_SCHEMA_ERROR_VALIDATE_NO_FIELDS',                            -41);
-define('MDB2_SCHEMA_ERROR_VALIDATE_NO_INDEX_FIELD_NAME',                  -42);
-define('MDB2_SCHEMA_ERROR_VALIDATE_NO_INDEX_NAME',                        -43);
-define('MDB2_SCHEMA_ERROR_VALIDATE_NO_SEQUENCE_NAME',                     -44);
-define('MDB2_SCHEMA_ERROR_VALIDATE_NO_TABLE_NAME',                        -45);
-define('MDB2_SCHEMA_ERROR_VALIDATE_PRIMARY_EXISTS',                       -46);
-define('MDB2_SCHEMA_ERROR_VALIDATE_PRIMARY_NULL',                         -47);
-define('MDB2_SCHEMA_ERROR_VALIDATE_SEQUENCE_EXISTS',                      -48);
-define('MDB2_SCHEMA_ERROR_VALIDATE_TABLE_EXISTS',                         -49);
-define('MDB2_SCHEMA_ERROR_VALIDATE_INDEX_FIELD_EXISTS',                   -50);
+define('MDB2_SCHEMA_ERROR_VALIDATE',                                -7);
 
 /**
  * The database manager is a class that provides a set of database
@@ -2341,6 +2298,7 @@ class MDB2_Schema extends PEAR
                 MDB2_SCHEMA_ERROR_UNSUPPORTED  => 'not supported',
                 MDB2_SCHEMA_ERROR_NOT_CAPABLE  => 'not capable',
                 MDB2_SCHEMA_ERROR_NODBSELECTED => 'no database selected',
+                MDB2_SCHEMA_ERROR_VALIDATE     => 'schema validation error',
             );
         }
 
