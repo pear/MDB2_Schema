1e48754ccc1cefa2030d334fa62d87ffc1cf53da
Igor Feghali <ifeghali@php.net>
2006-10-13 20:54:33 +0000

    - notes and todo updated for the next release
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2_Schema/trunk@221705 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -2,16 +2,19 @@
 
 require_once 'PEAR/PackageFileManager.php';
 
-$version = 'XXX';
+$version = '0.7.0';
 $notes = <<<EOT
-- fixed warning due to not checking with isset() in the parser
 - various fixes to the DTD
 - added new XSD schema definition
-- added support for DML statements
-- moved validation code out of the parser into a separate class
-- ensure all identifiers are passed to quoteIdentifier() (Bug #8429)
+- xml schema documentation updated
 - added new example parse.php/schema.xml to demo the DML support
-
+- improved main functionalities demo, example.php
+- added support for DML statements (UPDATE, DELETE, improved INSERT)
+- moved validation code out of the parser into a separate class
+- created an error code for writer
+- returning an error when fopen() fails in the writer
+- fixed warning due to not checking with isset() in the parser
+- ensure all identifiers are passed to quoteIdentifier() (Bug #8429)
 
 open todo items:
 - make MDB2_Schema loadable via MDB2_Driver_Common::loadModule() (Bug #8270)
@@ -20,9 +23,20 @@ open todo items:
   if it meets certain criterias based on that version number (or some other
   variable). this would enable people to add DML statements that are only
   executed when updating from a specific version.
-- add support for recursive tag parsing
+- add support for recursive tag parsing. Currently only expression-function
+  is supported but not expression-expression or function-function, although
+  recursion is already supported by the initializeTable method
+- parser should be replaced by XML serializer ?
+- modularize writer code or remake it
 - add specific error codes for validate class
 - add support for ORDER clauses on UPDATEs (to resolve the duplicate key problem)
+- update description.schema.xml
+- document how to use the API
+- create unit test for comparedefinitions
+- create unit test for initializetable
+- create unit test to compare the expected array definition with what is parsed
+- bind a parsed empty value to NULL or ''. currently it is generating a query error when creating a table from file
+- HTML entities aren't being parsed correctly
 EOT;
 
 $description =<<<EOT
@@ -64,7 +78,7 @@ if (PEAR::isError($result)) {
 }
 
 $package->addMaintainer('lsmith', 'lead', 'Lukas Kahwe Smith', 'smith@pooteeweet.org');
-$package->addMaintainer('ifeghali', 'developer', 'Igor Feghali', 'ifeghali@php.net');
+$package->addMaintainer('ifeghali', 'lead', 'Igor Feghali', 'ifeghali@php.net');
 
 $package->addDependency('php',     '4.2.0', 'ge',  'php', false);
 $package->addDependency('PEAR',    '1.0b1', 'ge',  'pkg', false);
