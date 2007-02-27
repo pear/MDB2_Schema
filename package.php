93fb63058f3d97d0506f331a279093e8a20929f8
Igor Feghali <ifeghali@php.net>
2007-02-27 23:20:12 +0000

    - release 0.7.1
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2_Schema/trunk@230905 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -4,45 +4,28 @@ require_once 'PEAR/PackageFileManager2.php';
 PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
 $version = '1.0.0';
-$version_api = '0.7.0';
-$version_release = '0.7.0';
+$version_api = '0.7.1';
+$version_release = '0.7.1';
 $state = 'beta';
 
 $notes = <<<EOT
-- Improved INSERT support, added UPDATE and DELETE statements
-- XML syntax redesigned to cover DML addictions
-- Creation of an explicit NULL (<null></null>)
-- various fixes to the DTD, including DML addictions
-- XSD schema definition created
-- Tests also output php version
-- New example parse.php lets you check how a XML schema is stored
-- Updated schema.xml to demo the DML support
-- Documentation reformulated, also covering new XML syntax
-- Improved demo script example.php, which now includes more actions and options
-- Writer is returning an error when fopen() fails
-- Writer has now its own error code
-- Variable "seq" became "sequence" and "seq_name" became "sequence_name" in the
-  Parser
-- Moved validation code out of the Parser into a new separate class called
-  Validation
-- Fixed index handling in the Parser
-- Refactored error handling in the Parser
-- Removed variable "init_field", that wasn't being used, from the Parser
-- Parser simulates only one level of recurssion, which means no more than 
-  function-expression or vice-versa
-- Fixed warning due to not checking with isset() in the Parser
-- Fixed warning due to not checking with isset() in the Schema
-- Typo fix (related to Bug #9024)
-- Fixed createDatabase() that was trying to use a non-existant database that 
-  was about to be created
-- Simplified API for compareTableFieldsDefinitions() and 
-  compareTableIndexesDefinitions()
-- Rewritten some docblocks
-- Added fold markers where missing
-- Removed @static from non static methods
-- Fixed several PEAR CS issues
-- Added code for field/identifier quoting
-- Ensure all identifiers are passed to quoteIdentifier() (Bug #8429)
+- Package converted to version 2
+- compareDefinition() was trying to update non-changing id field (Bug #8820)
+- for a moment <default> should be always parsed as string, no matter the field
+  type. this behavior may change in the future.
+- undefined method MDB2_Schema::quoteIdentifier() (Bug #9172)
+- added support for explicit NULL (<null></null>)
+- <value></value> is now correctly being parsed as an empty string
+- fixed internal bug in Parser that was introduced in revision 1.46 (Bug #9435)
+- MDB2_Schema_Parser::setData() has been removed
+- MDB2_Schema_Validate::validateDataField() is validating field data again
+  (Bug #9181)
+- updated reserved keywords of ibase
+- updated XML Schema documentation
+- updated XML Schema example
+- added a new test case to isBoolean() (empty string)
+- released the very first approach of the new parser that makes usage of
+  XML_Serializer (alternative parser)
 
 open todo items:
 - Make MDB2_Schema loadable via MDB2_Driver_Common::loadModule() (Bug #8270)
@@ -51,10 +34,6 @@ open todo items:
   if it meets certain criterias based on that version number (or some other
   variable). This would enable people to add DML statements that are only
   executed when updating from a specific version.
-- Add support for recursive tag parsing. Currently only expression<->function
-  is supported but not expression-expression or function-function, although
-  recursion is already supported by the initializeTable() method
-- Parser should be replaced for XML serializer ?
 - Modularize Writer code or remake it
 - Add specific error codes for Validate class
 - Add support for ORDER clauses on UPDATEs (to resolve the duplicate key 
@@ -66,10 +45,11 @@ open todo items:
 - Create unit test to compare the expected array definition with what is parsed
 - HTML entities aren't being parsed correctly
 - Add optional support for scale in decimal fields
-- Implement "WHERE column IS NULL"
-- Add ability to parse a "contents-only" XML dump
 - Improve validateDataFieldValue() to validate <column>
 - Provide more info on MDB2_Schema_Validate errors (output parsed value and expected value)
+- Views support
+- Foreign keys support
+- Fulltext index support
 EOT;
 
 $description = <<<EOT
