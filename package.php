e0557b87059acbf145ebc3bf471e244d28db1aec
Igor Feghali <ifeghali@php.net>
2007-08-20 16:38:59 +0000

    - release 0.8.0
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB2_Schema/trunk@241657 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -1,27 +1,79 @@
 <?php
+// +----------------------------------------------------------------------+
+// | PHP versions 4 and 5                                                 |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1998-2007 Lukas Smith, Igor Feghali                    |
+// | All rights reserved.                                                 |
+// +----------------------------------------------------------------------+
+// | MDB2 is a merge of PEAR DB and Metabases that provides a unified DB  |
+// | API as well as database abstraction for PHP applications.            |
+// | This LICENSE is in the BSD license style.                            |
+// |                                                                      |
+// | Redistribution and use in source and binary forms, with or without   |
+// | modification, are permitted provided that the following conditions   |
+// | are met:                                                             |
+// |                                                                      |
+// | Redistributions of source code must retain the above copyright       |
+// | notice, this list of conditions and the following disclaimer.        |
+// |                                                                      |
+// | Redistributions in binary form must reproduce the above copyright    |
+// | notice, this list of conditions and the following disclaimer in the  |
+// | documentation and/or other materials provided with the distribution. |
+// |                                                                      |
+// | Neither the name of Lukas Smith, Igor Feghali nor the names of his   |
+// | contributors may be used to endorse or promote products derived from |
+// | this software without specific prior written permission.             |
+// |                                                                      |
+// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
+// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
+// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
+// | FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE      |
+// | REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,          |
+// | INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
+// | BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS|
+// |  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED  |
+// | AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT          |
+// | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY|
+// | WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE          |
+// | POSSIBILITY OF SUCH DAMAGE.                                          |
+// +----------------------------------------------------------------------+
+// | Author: Igor Feghali <ifeghali@php.net>                              |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+//
 
 require_once 'PEAR/PackageFileManager2.php';
 PEAR::setErrorHandling(PEAR_ERROR_DIE);
 
-$version = '1.0.0';
-$version_api = '0.7.2';
-$version_release = '0.7.2';
+$version_api = '0.8.0';
+$version_release = '0.8.0';
 $state = 'beta';
 
 $notes = <<<EOT
-- Allow options to be passed in when creating tables (Bug #10278)
-- Existence of a single index results in no further index creation (Bug #10296)
-- MDB2_Schema::createTableIndexes() ignores indexes (Bug #10285)
-- column identifiers quoting problem (Bug #10195)
-- Handle 'scale' value in DECIMAL field definition (Bug #10475)
-- fixed phpdoc comment for validateDataField()
-- all primary keys are given an index name of "primary" (Bug #10457)
-- example.php should echo xml when no filename is given (Bug #10226)
+- Primary Key is removed when updating a table against its XML (Bug #11604)
+- Added support to INSERT...SELECT syntax, so it is now possible to prevent data loss when rollback an updated database (Bug #10892)
+- Updated API of getInstructionFields() and getInstructionWhere()
+- It is now possible to prevent updateDatabase() of overwriting the old schema file
+- alterDatabaseTables() first add new tables, then remove the old one. it is necessary to save table data before a remove
+- updateDatabase() was aborting, under certain conditions, when <was> was not found (Bug #11600)
+- Now validating database definition when reverse engineering (Bug #11604)
+- createDatabase() has a new parameter that is passed to the database driver to set some options like table engine
+- Foreign Keys support
+- It is now possible to assign to a column the value of another column, of the same table, when inserting data
+- Fixed the HTML etities issue when parsing a schema file (Bug #11676)
+- XML Documentation, XSD and DST updated to show/expect the attributes in a unique order. Writer and parsers was also updated to handle the attributes in that some order. That said Parser and Parser2 must have the exactly same output for a given XML
+- Except for table fields, all other database elements are being initialized with all its attributes, no matter if those attributes are present on the XML or not
+- Many bugs fixed in Parser2, that was not creating a valid database definition
+- Introduced attribute "fixed" of table field declaration
+- validateIndex() is now checking whether a index has fields
+- Validate is not returning MDB2_OK instead of boolean true
+- XML Documentation and schema validators revised and updated
 
 open todo items:
+- Clean up output of getDefinitionFromDatabase(). Sync it with Parser and Parser2.
 - Automatically load reserved keywords
 - Make MDB2_Schema loadable via MDB2_Driver_Common::loadModule() (Bug #8270)
-- Add ability to define variables inside the schema (like a version number)
 - Allow simple if statements that mean that anything enclosed is only executed
   if it meets certain criterias based on that version number (or some other
   variable). This would enable people to add DML statements that are only
@@ -31,15 +83,12 @@ open todo items:
 - Add support for ORDER clauses on UPDATEs (to resolve the duplicate key 
   problem)
 - Update description.schema.xml
-- Document how to use the API
 - Create unit test for comparedefinitions()
 - Create unit test for initializetable()
 - Create unit test to compare the expected array definition with what is parsed
-- HTML entities aren't being parsed correctly
 - Improve validateDataFieldValue() to validate <column>
 - Provide more info on MDB2_Schema_Validate errors (output parsed value and expected value)
 - Views support
-- Foreign keys support
 - Fulltext index support
 EOT;
 
@@ -87,3 +136,5 @@ if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 
 } else {
     $package->debugPackageFile();
 }
+
+?>
