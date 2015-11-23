f66425708e0b951da1e89d1bfed982bbdd9072f7
Daniel Convissor <danielc@analysisandsolutions.com>
2015-11-22 20:51:26 -0500

    Use __construct for XML_Parser.

--- MDB2/Schema/Parser.php
+++ MDB2/Schema/Parser.php
@@ -134,7 +134,7 @@ class MDB2_Schema_Parser extends XML_Parser
     ) {
         // force ISO-8859-1 due to different defaults for PHP4 and PHP5
         // todo: this probably needs to be investigated some more andcleaned up
-        parent::XML_Parser('ISO-8859-1');
+        parent::__construct('ISO-8859-1');
 
         $this->variables = $variables;
         $this->structure = $structure;
