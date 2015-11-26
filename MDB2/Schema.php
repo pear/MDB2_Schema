463350bc17c04765b9c16c90df6ac9687df88da3
Daniel Convissor <danielc@analysisandsolutions.com>
2015-11-26 09:13:36 -0500

    Make raiseError() and errorMessage() static.

--- MDB2/Schema.php
+++ MDB2/Schema.php
@@ -2674,7 +2674,7 @@ class MDB2_Schema extends PEAR
      * @return  string  error message, or false if the error code was not recognized
      * @access public
      */
-    function errorMessage($value = null)
+    public static function errorMessage($value = null)
     {
         static $errorMessages;
         if (is_array($value)) {
@@ -2726,7 +2726,7 @@ class MDB2_Schema extends PEAR
      * @access  public
      * @see PEAR_Error
      */
-    function &raiseError($code = null, $mode = null, $options = null, $userinfo = null)
+    public static function &raiseError($code = null, $mode = null, $options = null, $userinfo = null)
     {
         $err = PEAR::raiseError(null, $code, $mode, $options,
                                 $userinfo, 'MDB2_Schema_Error', true);
